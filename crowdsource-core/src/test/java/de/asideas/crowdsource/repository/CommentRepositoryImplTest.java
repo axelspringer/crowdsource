d552d0837042c4440e3b299bb78ee143267d77be
Liang Shi <liang.shi@asideas.de>
2016-01-18 16:08:01 +0100

    CROWD-162: added backend tests

--- crowdsource-core/src/test/java/de/asideas/crowdsource/repository/CommentRepositoryImplTest.java
+++ crowdsource-core/src/test/java/de/asideas/crowdsource/repository/CommentRepositoryImplTest.java
@@ -2,8 +2,11 @@ package de.asideas.crowdsource.repository;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
+import de.asideas.crowdsource.domain.model.ProjectEntity;
+import de.asideas.crowdsource.presentation.statistics.results.BarChartStatisticsResult;
 import de.asideas.crowdsource.presentation.statistics.results.LineChartStatisticsResult;
 import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -15,6 +18,7 @@ import org.springframework.data.mongodb.core.mapreduce.MapReduceResults;
 import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.Iterator;
+import java.util.List;
 
 import static de.asideas.crowdsource.domain.model.CommentEntity.COLLECTION_NAME;
 import static de.asideas.crowdsource.repository.CommentRepositoryImpl.CHART_NAME_SUM_COMMENTS;
@@ -24,7 +28,6 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,24 +38,32 @@ public class CommentRepositoryImplTest {
     @Mock
     private MongoTemplate mongoTemplate;
 
+    @Mock
+    private MapReduceResults<CommentRepositoryImpl.KeyValuePair> mockedMapReduceResult;
+
+    @Mock
+    private Iterator mockIterator;
+
     @InjectMocks
     private CommentRepositoryImpl commentRepository;
 
     private DateTime startDate;
     private DateTime endDate;
 
+    @Before
+    public void setUp() {
+        when(mongoTemplate.mapReduce(any(Query.class), eq(COLLECTION_NAME), anyString(), anyString(), eq(CommentRepositoryImpl.KeyValuePair.class)))
+                .thenReturn(mockedMapReduceResult);
+        when(mongoTemplate.mapReduce(eq(COLLECTION_NAME), anyString(), anyString(), eq(CommentRepositoryImpl.KeyValuePair.class)))
+                .thenReturn(mockedMapReduceResult);
+
+        when(mockedMapReduceResult.iterator()).thenReturn(mockIterator);
+    }
+
     @Test
     public void sumCommentsGroupByCreatedDate_should_return_empty_linechart_instance_although_no_data_from_db() throws Exception {
         DateTime startDate = DateTime.now().minusDays(1);
         DateTime endDate = DateTime.now();
-
-        MapReduceResults<CommentRepositoryImpl.KeyValuePair> mockedMapReduceResult = mock(MapReduceResults.class);
-
-        when(mongoTemplate.mapReduce(any(Query.class), eq(COLLECTION_NAME), anyString(), anyString(), eq(CommentRepositoryImpl.KeyValuePair.class)))
-                .thenReturn(mockedMapReduceResult);
-
-        Iterator mockIterator = mock(Iterator.class);
-        when(mockedMapReduceResult.iterator()).thenReturn(mockIterator);
 
         LineChartStatisticsResult result = commentRepository.sumCommentsGroupByCreatedDate(startDate, endDate);
 
@@ -70,13 +81,8 @@ public class CommentRepositoryImplTest {
 
         ArgumentCaptor<Query> captor = ArgumentCaptor.forClass(Query.class);
 
-        MapReduceResults<CommentRepositoryImpl.KeyValuePair> mockedMapReduceResult = mock(MapReduceResults.class);
-
         when(mongoTemplate.mapReduce(captor.capture(), eq(COLLECTION_NAME), anyString(), anyString(), eq(CommentRepositoryImpl.KeyValuePair.class)))
                 .thenReturn(mockedMapReduceResult);
-
-        Iterator mockIterator = mock(Iterator.class);
-        when(mockedMapReduceResult.iterator()).thenReturn(mockIterator);
 
         commentRepository.sumCommentsGroupByCreatedDate(startDate, endDate);
 
@@ -96,13 +102,6 @@ public class CommentRepositoryImplTest {
         DateTime startDate = DateTime.now().minusDays(1);
         DateTime endDate = DateTime.now();
 
-        MapReduceResults<CommentRepositoryImpl.KeyValuePair> mockedMapReduceResult = mock(MapReduceResults.class);
-
-        when(mongoTemplate.mapReduce(any(Query.class), eq(COLLECTION_NAME), anyString(), anyString(), eq(CommentRepositoryImpl.KeyValuePair.class)))
-                .thenReturn(mockedMapReduceResult);
-
-        Iterator mockIterator = mock(Iterator.class);
-        when(mockedMapReduceResult.iterator()).thenReturn(mockIterator);
         CommentRepositoryImpl.KeyValuePair mockResult1 = new CommentRepositoryImpl.KeyValuePair("1453037072969", 3L);
         CommentRepositoryImpl.KeyValuePair mockResult2 = new CommentRepositoryImpl.KeyValuePair("1452898800000", 1L);
 
@@ -127,13 +126,6 @@ public class CommentRepositoryImplTest {
         startDate = DateTime.now().minusDays(1);
         endDate = DateTime.now();
 
-        MapReduceResults<CommentRepositoryImpl.KeyValuePair> mockedMapReduceResult = mock(MapReduceResults.class);
-
-        when(mongoTemplate.mapReduce(any(Query.class), eq(COLLECTION_NAME), anyString(), anyString(), eq(CommentRepositoryImpl.KeyValuePair.class)))
-                .thenReturn(mockedMapReduceResult);
-
-        Iterator mockIterator = mock(Iterator.class);
-        when(mockedMapReduceResult.iterator()).thenReturn(mockIterator);
         CommentRepositoryImpl.KeyValuePair mockResult = new CommentRepositoryImpl.KeyValuePair("test_abc", 1L);
 
         when(mockIterator.hasNext()).thenReturn(true, false);
@@ -141,9 +133,47 @@ public class CommentRepositoryImplTest {
 
         commentRepository.sumCommentsGroupByCreatedDate(startDate, endDate);
     }
-//
-//    @Test
-//    public void countCommentsGroupByProject_should_sort_result() {
-//
-//    }
+
+    @Test
+    public void countCommentsGroupByProject_should_sort_result() {
+
+        CommentRepositoryImpl.KeyValuePair mockResult1 = new CommentRepositoryImpl.KeyValuePair("test_a", 1L);
+        CommentRepositoryImpl.KeyValuePair mockResult2 = new CommentRepositoryImpl.KeyValuePair("test_b", 100L);
+        CommentRepositoryImpl.KeyValuePair mockResult3 = new CommentRepositoryImpl.KeyValuePair("test_c", 10L);
+
+        when(mockIterator.hasNext()).thenReturn(true, true, true, false);
+        when(mockIterator.next()).thenReturn(mockResult1, mockResult2,mockResult3);
+
+        when(mongoTemplate.findOne(any(Query.class), eq(ProjectEntity.class))).thenReturn(new ProjectEntity());
+
+        List<BarChartStatisticsResult> result = commentRepository.countCommentsGroupByProject(5);
+
+        assertThat(result.get(0).getCount(), is(100L));
+        assertThat(result.get(1).getCount(), is(10L));
+        assertThat(result.get(2).getCount(), is(1L));
+    }
+
+    @Test
+    public void countCommentsGroupByProject_should_limit_result() {
+        CommentRepositoryImpl.KeyValuePair mockResult1 = new CommentRepositoryImpl.KeyValuePair("test_a", 1L);
+        CommentRepositoryImpl.KeyValuePair mockResult2 = new CommentRepositoryImpl.KeyValuePair("test_b", 100L);
+        CommentRepositoryImpl.KeyValuePair mockResult3 = new CommentRepositoryImpl.KeyValuePair("test_c", 10L);
+        CommentRepositoryImpl.KeyValuePair mockResult4 = new CommentRepositoryImpl.KeyValuePair("test_e", 18L);
+        CommentRepositoryImpl.KeyValuePair mockResult5 = new CommentRepositoryImpl.KeyValuePair("test_f", 17L);
+        CommentRepositoryImpl.KeyValuePair mockResult6 = new CommentRepositoryImpl.KeyValuePair("test_g", 21L);
+
+        when(mockIterator.hasNext()).thenReturn(true, true, true, true, true, true, false);
+        when(mockIterator.next()).thenReturn(mockResult1, mockResult2,mockResult3, mockResult4, mockResult5, mockResult6);
+
+        when(mongoTemplate.findOne(any(Query.class), eq(ProjectEntity.class))).thenReturn(new ProjectEntity());
+
+        List<BarChartStatisticsResult> result = commentRepository.countCommentsGroupByProject(5);
+
+        assertThat(result.size(), is(5));
+        assertThat(result.get(0).getCount(), is(100L));
+        assertThat(result.get(1).getCount(), is(21L));
+        assertThat(result.get(2).getCount(), is(18L));
+        assertThat(result.get(3).getCount(), is(17L));
+        assertThat(result.get(4).getCount(), is(10L));
+    }
 }
