417f09e81b35beb1d5a07ac017b3b36d6bad35e3
Stefan Bieler <stefan.bieler@asideas.de>
2016-01-20 11:06:34 +0100

    CROWD-89: Resolves CROWD-90 and CROWD-91: Show git and maven information in info endpoint.

--- crowdsource-core/src/main/java/de/asideas/crowdsource/CrowdSource.java
+++ crowdsource-core/src/main/java/de/asideas/crowdsource/CrowdSource.java
@@ -7,9 +7,12 @@ import de.asideas.crowdsource.config.mail.MailSenderConfig;
 import de.asideas.crowdsource.config.mail.MailTemplateConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -20,6 +23,11 @@ import java.util.List;
 @Configuration
 @Import({ThymeleafAutoConfiguration.class, MongoDBConfig.class, SecurityConfig.class, MailSenderConfig.class, MailTemplateConfig.class, SchedulerConfig.class})
 @ComponentScan(basePackages = "de.asideas.crowdsource", excludeFilters = @ComponentScan.Filter(Configuration.class))
+@PropertySources({
+    @PropertySource(value = "build.properties", ignoreResourceNotFound = true),
+    @PropertySource(value = "build.core.properties", ignoreResourceNotFound = false)
+})
+@EnableConfigurationProperties()
 public class CrowdSource extends WebMvcConfigurerAdapter {
 
     @Autowired(required = false)
